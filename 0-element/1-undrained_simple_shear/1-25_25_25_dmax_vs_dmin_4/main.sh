f1dc1423b97def51d0b6f104ff3698091fbdbb34
Ming Yang <myang@Mings-MBP.hitronhub.home>
2021-08-05 22:46:15 -0700

    0805-ss_1_ready ss_2_modifying

---
+++ 0-element/1-undrained_simple_shear/1-25_25_25_dmax_vs_dmin_4/main.sh
@@ -0,0 +1,17 @@
+start=`date +%s`
+#
+#mpirun -np 2 /Users/myang/Documents/1-Programming/LIGGGHTS-PUBLIC/src/lmp_auto < 0-iso_comp.lmp
+#mpirun -np 2 /Users/myang/Documents/1-Programming/LIGGGHTS-PUBLIC/src/lmp_auto < 1-iso_comp.lmp
+#mpirun -np 2 /Users/myang/Documents/1-Programming/LIGGGHTS-PUBLIC/src/lmp_auto < 2-iso_comp_bp.lmp
+#mpirun -np 2 /Users/myang/Documents/1-Programming/LIGGGHTS-PUBLIC/src/lmp_auto < 3-iso_comp_bp.lmp
+mpirun -np 2 /Users/myang/Documents/1-Programming/LIGGGHTS-PUBLIC/src/lmp_auto < 4-undrained_simple_shear_bp.lmp
+#
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo "The running time is ${runtime}s"
+echo "The running time is ${runtime}s"
+echo "The running time is ${runtime}s"
+echo "The running time is ${runtime}s"
+echo "The running time is ${runtime}s"
